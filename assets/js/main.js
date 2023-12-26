8cb00bf637760c331f5a7c78fd983be2ee4cb1a6
abdulalimirzayi01 <abdulalialimirzayi@gmail.com>
2023-12-26 21:32:07 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// let images = ["assets/images/photo1686055769.jpeg","assets/images/office.jpg"]; 
+// let mainImg = document.getElementById("location-slider");
+
+
+// let num = 0;
+// function next() {
+//     num++;
+//     if (num >= images.length) {
+//         num = 0;
+//     }
+//     mainImg.src = images[num];
+// }
+
+// function back() {
+//     num--;
+//     if (num < 0) {
+//         num = images.length - 1;
+//     }
+//     mainImg.src = images[num];
+// }
+// document.getElementById("next").addEventListener("click", next);
+// document.getElementById("back").addEventListener("click", back);
+
+
+// const auto = true;
+// const intervalTime = 1500;
+// let slideInterval;
+
+// // if (auto) {
+// //     slideInterval = setInterval(next, intervalTime);
+// // }
